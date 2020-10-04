9bca261c386e8a401ad2e99d9065e9f35b899592
JsonTom888 <Good666888@163.com>
2020-10-04 13:05:56 +0800

    定时任务

---
+++ quartzdemo/src/test/java/com/example/quartzdemo/QuartzdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartzdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
